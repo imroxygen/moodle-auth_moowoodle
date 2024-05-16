0e171fdc9a52b0c9f20b3806a1a8adef7d35e00a
jayanta <jayantabarik7022@gmail.com>
2024-05-16 14:07:27 +0530

    external services implemented

--- db/services.php
+++ db/services.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- *
+ * Services Api
  * @package     auth_moowoodle
  * @author      DualCube <admin@dualcube.com>
  * @copyright   Dualcube (https://dualcube.com)
